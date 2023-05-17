90ef91cec622a1208e54121aab2609a7b5d4d154
João Peixoto <joao@jpex.dev>
2023-05-17 15:54:11 +0100

    navbar2
    navbar completa, falta ligações

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
 <link rel="stylesheet" href="css/custom.css" />
 <header class="header">
     <nav class="navbar">
-        <a href="#" class="logo">JPex.Dev</a>
+        <a href="home.php" class="logo">JPex.Dev</a>
         <ul class="nav-menu">
-            <li class="nav-item"> <a href="#" class="nav-link">Home</a></li>
+            <li class="nav-item"> <a href="project.php" class="nav-link"> Projetos</a></li>
             <li class="nav-item"><a href="#" class="nav-link">Sobre</a></li>
             <li class="nav-item"><a href="#" class="nav-link">Contato</a></li>
         </ul>
@@ -12,7 +12,15 @@
             <span class="bar"></span>
             <span class="bar"></span>
         </div>
+        <script>
+            const hamburguer = document.querySelector(".hamburguer")
+            const navMenu =  document.querySelector(".nav-menu");
 
+            hamburguer.addEventListener("click", ()=>{
+                hamburguer.classList.toggle('active');
+                navMenu.classList.toggle('active');
+            })
+        </script>
 
     </nav>
 </header>
