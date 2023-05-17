194e1049e880e28a29052e4b0f1324ea89b50b7f
João Peixoto <joao@jpex.dev>
2023-05-17 15:22:28 +0100

    navbar1
    not completed

---
+++ header.php
@@ -0,0 +1,18 @@
+<link rel="stylesheet" href="css/custom.css" />
+<header class="header">
+    <nav class="navbar">
+        <a href="#" class="logo">JPex.Dev</a>
+        <ul class="nav-menu">
+            <li class="nav-item"> <a href="#" class="nav-link">Home</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Sobre</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Contato</a></li>
+        </ul>
+        <div class="hamburguer">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+        </div>
+
+
+    </nav>
+</header>
