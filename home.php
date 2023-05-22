af0d3ccc26e8d902e17a67575cd8fb172412788a
João Peixoto <joao@jpex.dev>
2023-05-22 12:16:18 +0100

    mobile adapted

--- home.php
+++ home.php
@@ -12,20 +12,21 @@
 
 <body>
     <div class="landing">
-        <div class="land-let">
-            <div class="let">
-                <div class="big-letter">
+
+        <div class="land-image">
+
+            <img src="images/SAM_1460-1.jpg" alt="me" class="mobile">
+        </div>
+        <div class="land-let mobile">
+            <div class="let mobile">
+                <div class="big-let">
                     "Today better than yesterday, Tomorrow better than today."
                 </div>
                 <div class="me-let">
-                    wefrwfwf
+                    Hi! Im a 19y developer. A AI explorer. Coding learner.
 
                 </div>
             </div>
-        </div>
-        <div class="land-image">
-
-            <img src="images/SAM_1460-1.jpg">
         </div>
     </div>
 </body>
